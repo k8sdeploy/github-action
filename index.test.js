ab0b2b9c4ec951810b98e998583d918b1eaf93e2
Keloran <keloran@develbox.info>
2022-09-12 23:18:57 +0100

    hmm

--- index.test.js
+++ index.test.js
@@ -5,9 +5,9 @@ const path = require('path');
 
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-  process.env['INPUT_K8SDEPLOY-KEY'] = 'dummy-key';
-  process.env['INPUT_K8SDEPLOY-SECRET'] = 'dummy-secret';
-  process.env['INPUT_K8SDEPLOY-ID'] = 'dummy-id';
+  process.env["INPUT_K8SDEPLOY-KEY"] = 'dummy-key';
+  process.env["INPUT_K8SDEPLOY-SECRET"] = 'dummy-secret';
+  process.env["INPUT_K8SDEPLOY-ID"] = 'dummy-id';
 
   console.log("process.env", process.env)
 
